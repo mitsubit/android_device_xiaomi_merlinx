ecd7547b6bb43ad3a1f1d05450b1638ef75caeeb
mitsubit <139018525+mitsubit@users.noreply.github.com>
2023-07-16 15:08:33 +0700

    Havoc-a13 initial

--- lineage_merlinx.mk
+++ lineage_merlinx.mk
@@ -14,6 +14,25 @@ $(call inherit-product, device/xiaomi/merlinx/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Some stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_DISABLE_EPPE := true
+TARGET_HAS_UDFPS := false
+
+# Maintainer
+HAVOC_BUILD_TYPE := Unofficial
+HAVOC_MAINTAINER := mitsuaa
+HAVOC_GROUP_URL := https://t.me/mitsuaa
+
+# Pixel customization
+TARGET_SUPPORTS_GOOGLE_RECORDER ?= true
+TARGET_INCLUDE_STOCK_ARCORE ?= true
+TARGET_INCLUDE_LIVE_WALLPAPERS ?= true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+
 PRODUCT_NAME := lineage_merlinx
 PRODUCT_DEVICE := merlinx
 PRODUCT_MANUFACTURER := Xiaomi
