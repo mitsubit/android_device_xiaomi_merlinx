32dc67b2de37faf72f73d81082654ef397ba75dc
mitsubit <139018525+mitsubit@users.noreply.github.com>
2023-08-01 16:15:26 +0700

    Try to move

--- lineage_merlinx.mk
+++ lineage_merlinx.mk
@@ -8,11 +8,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from device makefile.
-$(call inherit-product, device/xiaomi/merlinx/device.mk)
-
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/xiaomi/merlinx/device.mk)
 
 # SOC
 PROCESSOR_MODEL := mt6768
